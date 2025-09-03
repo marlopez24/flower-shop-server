78fdacf069431c26a858663d7af97324af25e43c
Marlyn <marlynlopezz@gmail.com>
2025-09-02 23:07:38 -0700

    created database connection with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -11,25 +12,61 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+mongoose.connection.once("open", async () => {
+  console.log("✅ Connected to MongoDB, testing query...");
+
+  const collections = await mongoose.connection.db.listCollections().toArray();
+  console.log(
+    "Collections:",
+    collections.map((c) => c.name)
+  );
+});
+
 // Test route
 app.get("/", (req, res) => {
   res.send("Server running...");
 });
 
-// Orders storage (in-memory for now)
-let orders = [];
+// Orders model
+const orderSchema = new mongoose.Schema({
+  customer: {
+    name: String,
+    email: String,
+    address: String,
+  },
+  cart: [{ id: Number, name: String, price: Number }],
+  status: { type: String, default: "new " },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Order = mongoose.model("Order", orderSchema);
 
 // Route to create new order
-app.post("/api/orders", (req, res) => {
-  const order = req.body; // frontend sends cart + customer info
-  order.id = Date.now(); // simple unique id
-  orders.push(order);
-  res.status(201).json({ message: "Order received", order });
+app.post("/api/orders", async (req, res) => {
+  try {
+    const newOrder = new Order(req.body);
+    const savedOrder = await newOrder.save();
+    res.status(201).json({ message: "Order received", order: savedOrder });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to create order" });
+  }
 });
 
 // Route to get all orders
-app.get("/api/orders", (req, res) => {
-  res.json(orders);
+app.get("/api/orders", async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.json(orders);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to retrieve orders" });
+  }
 });
 
 // Start server
